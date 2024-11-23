d2610d810ead55ac4a9ae818770fee0f3200481d
Sanjit Thangarasu <isanjit3@gmail.com>
2024-11-23 12:34:24 -0800

    Add sample responses

--- backend/main.py
+++ backend/main.py
@@ -52,11 +52,28 @@ async def upload_dataset(name: Optional[str] = Form(None), file: UploadFile = Fi
         "message": "Dataset uploaded successfully",
         "data": response.data[0]
     }
+    """
+    {
+        "message": "Dataset uploaded successfully",
+        "data": {
+            "id": "85d16a6d-d927-4e44-a876-e9742125b763",
+            "name": "sample_dataset.json",
+            "dataset": [
+                {
+                    "input": "John moved to the kitchen. Mary is in the garden. Where is John?",
+                    "target": "kitchen"
+                },
+                /* rest of the tasks */
+            ]
+        }
+    }
+    """
 
 
 @app.post("/benchmark", status_code=201)
 async def benchmark():
     logger.info("Running benchmark test")
+
     # ===================
     #  Run the benchmark
     # ===================
@@ -67,19 +84,24 @@ async def benchmark():
         benchmark_data = json.load(file)
 
     # Insert data into benchmark_results table
-    result_response = supabase.table("benchmark_results").insert(benchmark_results[0]).execute()
+    supabase.table("benchmark_results").insert(benchmark_results[0]).execute()
     logger.info("Inserted benchmark results into benchmark_results table")
     
     # Insert data into benchmark_data table
     for i, run in enumerate(benchmark_data):
-        run_data_response = supabase.table("benchmark_data").insert(run).execute()
+        supabase.table("benchmark_data").insert(run).execute()
         logger.info(f"Inserted benchmark data for run {i+1} into benchmark_data table")
     
     return {
         "message": "Benchmark test completed",
         "run_id": benchmark_results[0]["run_id"]
     }
-
+    """
+    {
+        "message": "Benchmark test completed",
+        "run_id": "GFJUCeSDrcsJMzch5HZ5an"
+    }
+    """
 
 @app.get("/benchmark/{run_id}", status_code=200)
 async def read_benchmark(run_id: str):
@@ -90,6 +112,23 @@ async def read_benchmark(run_id: str):
         "message": f"Benchmark results for {run_id}",
         "data": query_response.data[0]
         }
+    """
+    {
+        "message": "Benchmark results for GFJUCeSDrcsJMzch5HZ5an",
+        "data": {
+            "id": "b193dab8-b794-4ed0-a3c8-0a1f581fde38",
+            "run_id": "GFJUCeSDrcsJMzch5HZ5an",
+            "model": "openai/gpt-4",
+            "prompt": "{prompt}",
+            "accuracy": 0.79,
+            "stderr": 0.0409360181,
+            "run_start": "2024-11-23T00:27:58-08:00",
+            "run_end": "2024-11-23T00:40:23-08:00",
+            "upper_bound": 0.8702345954,
+            "lower_bound": 0.7097654046
+        }
+    }
+    """
 
 @app.get("/")
 async def read_root():
