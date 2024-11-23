e6ae518211eba2d44499d504df42778a62470fb4
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2024-11-22 22:56:34 -0800

    choked lib file removed from gitignore

--- frontend/app/countries/page.tsx
+++
@@ -1,9 +0,0 @@
-  import { createClient } from '@/utils/supabase/server';
-  import React from "react";
-
-  export default async function Countries() {
-    const supabase = await createClient();
-    const { data: countries } = await supabase.from("countries").select();
-
-    return <pre>{JSON.stringify(countries, null, 2)}</pre>
-  }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default async function Index() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Ratemyeval</h2>
+        <Button className="font-medium text-xl mb-4">
+            Test Benchmark
+        </Button>
       </main>
     </>
   );
